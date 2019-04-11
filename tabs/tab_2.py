079ea9cbcc5c4b0e2250359c0d776ad0defcf780
Lasseter <alasseter@deloitte.com>
2019-04-11 17:52:24 -0400

    Chuck Norris can believe it's not butter

--- tabs/tab_2.py
+++ tabs/tab_2.py
@@ -1,7 +1,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+# from dash.dependencies import Input, Output
 
 tab_2_layout = html.Div([
     html.H1('Page 2'),
