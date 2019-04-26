3dbed6c302f9f14f0e8cbd10aada76399c24a590
Austin Lasseter <austinlasseter@gmail.com>
2019-04-26 05:52:30 -0500

    I told my friend 10 jokes to get him to laugh. Sadly, no pun in 10 did.

--- application.py
+++ application.py
@@ -1,10 +1,11 @@
 import dash
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import dash_html_components as html
 import dash_core_components as dcc
 import plotly.graph_objs as go
 import pandas as pd
-from tabs import tab_1, tab_2, tab_3
+import numpy as np
+from tabs import tab_1, tab_2, tab_3, tab_4
 from utils import display_eval_metrics, Viridis
 
 
@@ -18,30 +19,35 @@ app.config['suppress_callback_exceptions'] = True
 app.css.append_css({
     'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'
 })
+app.title='Titanic!'
+
 
 ## Layout
 app.layout = html.Div([
     html.H1('Surviving the Titanic'),
-    dcc.Tabs(id="tabs-example", value='tab-1-example', children=[
-        dcc.Tab(label='Introduction', value='tab-1-example'),
-        dcc.Tab(label='Model Evaluation', value='tab-2-example'),
-        dcc.Tab(label='Testing Results', value='tab-3-example'),
+    dcc.Tabs(id="tabs-template", value='tab-1-template', children=[
+        dcc.Tab(label='Introduction', value='tab-1-template'),
+        dcc.Tab(label='Model Evaluation', value='tab-2-template'),
+        dcc.Tab(label='Testing Results', value='tab-3-template'),
+        dcc.Tab(label='User Inputs', value='tab-4-template'),
     ]),
-    html.Div(id='tabs-content-example')
+    html.Div(id='tabs-content-template')
 ])
 
 
 ############ Callbacks
 
-@app.callback(Output('tabs-content-example', 'children'),
-              [Input('tabs-example', 'value')])
+@app.callback(Output('tabs-content-template', 'children'),
+              [Input('tabs-template', 'value')])
 def render_content(tab):
-    if tab == 'tab-1-example':
+    if tab == 'tab-1-template':
         return tab_1.tab_1_layout
-    elif tab == 'tab-2-example':
+    elif tab == 'tab-2-template':
         return tab_2.tab_2_layout
-    elif tab == 'tab-3-example':
+    elif tab == 'tab-3-template':
         return tab_3.tab_3_layout
+    elif tab == 'tab-4-template':
+        return tab_4.tab_4_layout
 
 # Tab 2 callbacks
 
@@ -51,15 +57,15 @@ def radio_results(value):
     return display_eval_metrics(value)
 
 # Tab 3 callback # 1
-@app.callback(dash.dependencies.Output('page-3-content', 'children'),
-              [dash.dependencies.Input('page-3-dropdown', 'value')])
+@app.callback(Output('page-3-content', 'children'),
+              [Input('page-3-dropdown', 'value')])
 def page_3_dropdown(value):
     name=df.loc[value, 'Name']
     return f'You have selected "{name}"'
 
 # Tab 3 callback # 2
-@app.callback(dash.dependencies.Output('survival-prob', 'children'),
-              [dash.dependencies.Input('page-3-dropdown', 'value')])
+@app.callback(Output('survival-prob', 'children'),
+              [Input('page-3-dropdown', 'value')])
 def page_3_survival(value):
     survival=df.loc[value, 'survival_prob']
     actual=df.loc[value, 'Survived']
@@ -67,8 +73,8 @@ def page_3_survival(value):
     return f'Predicted probability of survival is {survival}%, Actual survival is {actual}'
 
 # Tab 3 callback # 2
-@app.callback(dash.dependencies.Output('survival-characteristics', 'children'),
-              [dash.dependencies.Input('page-3-dropdown', 'value')])
+@app.callback(Output('survival-characteristics', 'children'),
+              [Input('page-3-dropdown', 'value')])
 def page_3_characteristics(value):
     mydata=df.drop(['Survived', 'survival_prob', 'Name'], axis=1)
     return html.Table(
@@ -77,6 +83,64 @@ def page_3_characteristics(value):
             html.Td(mydata.iloc[value][col]) for col in mydata.columns
         ])]
     )
+
+# Tab 4 Callback # 1
+@app.callback(Output('user-inputs-box', 'children'),
+            [
+              Input('family_dropdown', 'value'),
+              Input('age_dropdown', 'value'),
+              Input('cabin_dropdown', 'value'),
+              Input('title_radio', 'value'),
+              Input('sex_radio', 'value'),
+              Input('port_radio', 'value')
+              ])
+def update_user_table(family, age, cabin, title, sex, embark):
+    return html.Div([
+        html.Div(f'Family Members: {family}'),
+        html.Div(f'Age: {age}'),
+        html.Div(f'Cabin Class: {cabin}'),
+        html.Div(f'Title: {title}'),
+        html.Div(f'Sex: {sex}'),
+        html.Div(f'Embarkation: {embark}'),
+    ])
+
+# Tab 4 Callback # 2
+@app.callback(Output('final_prediction', 'children'),
+            [
+              Input('family_dropdown', 'value'),
+              Input('age_dropdown', 'value'),
+              Input('cabin_dropdown', 'value'),
+              Input('title_radio', 'value'),
+              Input('sex_radio', 'value'),
+              Input('port_radio', 'value')
+              ])
+def final_prediction(family, age, cabin, title, sex, embark):
+    inputs=[family, age, cabin, title, sex, embark]
+    keys=['family', 'age', 'cabin', 'title', 'sex', 'embark']
+    dict6=dict(zip(keys, inputs))
+    df=pd.DataFrame([dict6])
+    df['age']=pd.to_numeric(df.age, errors='coerce')
+    df['third']=np.where(df.cabin=='Third',1,0)
+    df['second']=np.where(df.cabin=='Second',1,0)
+    df['female']=np.where(df.sex=='Female',1,0)
+    df['cherbourg']=np.where(df.embark=='Cherbourg',1,0)
+    df['queenstown']=np.where(df.embark=='Queenstown',1,0)
+    df['age2028']=np.where((df.age>=20)&(df.age<28))
+    df['age2838']=np.where((df.age>=28)&(df.age<38))
+    df['age3880']=np.where((df.age>=38)&(df.age<80))
+    df['mrs']=np.where(df.title=='Mrs.', 1,0)
+    df['miss']=np.where(df.title=='Miss', 1,0)
+    df['vip']=np.where(df.title=='VIP', 1,0)
+    df=df.drop(['family', 'age', 'cabin', 'title', 'sex', 'embark'], axis=1)
+    return(list(df.columns))
+
+
+
+
+
+
+
+
 
 ####### Run the app #######
 if __name__ == '__main__':
