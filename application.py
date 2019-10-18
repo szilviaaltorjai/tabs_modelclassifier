eab1b0938da4a4651c0fe4d990cb14fdc082ca4c
szilviaaltorjai <szilviaaltorjai@gmail.com>
2019-10-18 12:18:10 -0400

    deploy again

--- application.py
+++ application.py
@@ -16,10 +16,7 @@ app.config['suppress_callback_exceptions'] = True
 app.css.append_css({
     'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'
 })
-colors = {
-    'background': '#111111',
-    'text': '#7FDBFF'
-}
+
 app.title='Diabetes!'
 
 
